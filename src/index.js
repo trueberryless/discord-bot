a545414da5cc58ff9b2c5d8356da479a996f4466
Programmierer281986 <f.schneider@htlkrems.at>
2022-10-08 21:49:46 +0200

    next step

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 require("dotenv").config();
-const { Client } = require("discord.js");
+const fs = require("fs");
+const { Client, Collection } = require("discord.js");
 
 const client = new Client({ intents: [] });
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync("./src/commands").filter((file) => file.endsWith(".js"));
+
+commandFiles.forEach((commandFile) => {
+    const command = require(`./commands/${commandFile}`);
+    client.commands.set(command.data.name, command);
+});
 
 client.once("ready", () => {
     console.log(`Ready! Logged in as ${client.user.tag}! I'm on ${client.guilds.cache.size}`);
-    // client.user.setActivity({ name: "mit Code", type: "PLAYING" });
+    client.user.setActivity({ name: "mit Code", type: "PLAYING" });
+});
+
+client.on("interaction", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (command) {
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+
+            if (interaction.deffered || interaction.replied) {
+                interaction.editReply("Es ist ein Fehler beim Ausführen aufgetreten!");
+            } else {
+                interaction.reply("Es ist ein Fehler beim Ausführen aufgetreten!");
+            }
+        }
+    }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
